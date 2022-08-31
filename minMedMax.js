90b72237f70fbda7202b43fb13554a2173823fda
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-31 10:25:35 +0700

    quotation2

--- minMedMax.js
+++ minMedMax.js
@@ -4,7 +4,7 @@ function minMedMax(n1, n2, n3) {
   //code here
   min = Math.min(n1,n2,n3)
   max = Math.max(n1,n2,n3)
-  mid = n1 != min && n1 != max? n1 : n2 != min && n2 != max? n2 : n3 != min && n3 != max? n3 : undefined
-  return `{\"max\": ${max}, \"mid\": ${mid}, \"min\": ${min}}`
+  mid = n1 !== min && n1 !== max? n1 : n2 !== min && n2 !== max? n2 : n3 !== min && n3 !== max? n3 : undefined
+  return `{"max": ${max}, "mid": ${mid}, "min": ${min}}`
 }
 module.exports = minMedMax

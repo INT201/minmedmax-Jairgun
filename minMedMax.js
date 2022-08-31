cc3a5e45a546e129bd94a5e0841690125adcead2
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-31 09:55:48 +0700

    test1

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,9 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  min = Math.min(n1,n2,n3)
+  max = Math.max(n1,n2,n3)
+  med = n1 != min && n1 != max? n1 : n2 != min && n2 != max? n2 : n3 != min && n3 != max? n3 : undefined
+  return '{ min: '+ min +', mid: '+ med +' max:'+ max +' }'
 }
 module.exports = minMedMax
